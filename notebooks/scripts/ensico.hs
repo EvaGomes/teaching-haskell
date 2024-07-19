5167700aa8fb79e30fb9c103423b6bd076f098d6
egovas <egovas@outlook.pt>
2024-07-19 07:49:39 +0100

    Change ensico.hs to define operator >>

--- notebooks/scripts/ensico.hs
+++ notebooks/scripts/ensico.hs
@@ -1,13 +1,16 @@
 module ENSICO (module ENSICO,
+               module Prelude,
                module Data.Char,
                module Data.List) where
 
 ----------------------------------
 --- Import 3rd-party libraries ---
 
+import Prelude hiding ((>>))
 import Data.Char
 import Data.List
 
+done = putStrLn "Feito!"
 
 -------------------
 --- Operator |> ---
@@ -29,5 +32,10 @@ get i list = list !! i
 --- Function pp (Pretty-Printing) ---
 
 pp = putStr
+
+-------------------
+--- Operator >> ---
+
+(>>) = flip (.)
 
 
